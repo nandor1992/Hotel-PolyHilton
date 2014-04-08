e6bb26d987df71aa51dd05e9df6d99616ded18ba
ghisa <ghisa.ioachim@yahoo.com>
2014-04-08 18:33:54 +0300

    s Ghisa

--- PoliHilton/PoliHilton/Form1.Designer.cs
+++ PoliHilton/PoliHilton/Form1.Designer.cs
@@ -35,12 +35,13 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(108, 143);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "Button";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -51,6 +52,7 @@
             this.button2.TabIndex = 0;
             this.button2.Text = "Button";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -59,7 +61,7 @@
             this.ClientSize = new System.Drawing.Size(260, 256);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
