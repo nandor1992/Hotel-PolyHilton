844aa808bf09387621ab5a410a71147fddaf509e
tudorbala <beny_tudor@yahoo.ca>
2014-04-08 18:47:49 +0300

    am schimbat textul

--- PoliHilton/PoliHilton/tudor_test.Designer.cs
+++ PoliHilton/PoliHilton/tudor_test.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(40, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "It works!";
+            this.label1.Text = "It works!n jsnsjkasnkjanhdkja";
             // 
             // tudor_test
             // 
