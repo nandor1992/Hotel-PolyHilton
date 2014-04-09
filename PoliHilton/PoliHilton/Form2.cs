6898c77548246491839062f45d89bfd280dbc3e0
Nandor Verba <nandor1992@yahoo.com>
2014-04-09 13:58:35 +0300

    Form2

---
+++ PoliHilton/PoliHilton/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void adToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modificationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void adsadaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statisticToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void asdsaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void discountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
