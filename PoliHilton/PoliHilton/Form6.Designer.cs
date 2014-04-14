85a23601e53894437577333bcfbbb6477329e45c
iza92 <iza.birs@yahoo.com>
2014-04-14 21:29:18 +0300

    Am legat WPF in WinForm si am facut un buton in forma de camera (318). Nu va arata asa backgroundul, am vrut sa vad doar daca pot face butoanele

--- PoliHilton/PoliHilton/Form6.Designer.cs
+++ PoliHilton/PoliHilton/Form6.Designer.cs
@@ -33,24 +33,23 @@ namespace PoliHilton
         private void InitializeComponent()
         {
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
-            HostingWpfUserControlInWf.UserControl1 uc =new HostingWpfUserControlInWf.UserControl1();
+            this.uc = new HostingWpfUserControlInWf.UserControl1();
             this.SuspendLayout();
             // 
             // elementHost1
             // 
-            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.elementHost1.Location = new System.Drawing.Point(0, 0);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(511, 352);
+            this.elementHost1.Size = new System.Drawing.Size(686, 454);
             this.elementHost1.TabIndex = 0;
             this.elementHost1.Text = "elementHost1";
-            this.elementHost1.Child = uc;
+            this.elementHost1.Child = this.uc;
             // 
             // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 352);
+            this.ClientSize = new System.Drawing.Size(683, 419);
             this.Controls.Add(this.elementHost1);
             this.Name = "Form6";
             this.Text = "Booking";
@@ -62,5 +61,6 @@ namespace PoliHilton
         #endregion
 
         private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private HostingWpfUserControlInWf.UserControl1 uc;
     }
 }
