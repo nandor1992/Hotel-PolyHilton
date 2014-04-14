7f940b9c863049071125f98f1da32f15ba4e2eef
iza92 <iza.birs@yahoo.com>
2014-04-14 21:45:22 +0300

    Booking class with it's public methods

---
+++ PoliHilton/PoliHilton/Booking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This class is used for Form6(iza)
+
+namespace PoliHilton
+{   
+    class Booking
+    {
+        Database database;
+        Users u;
+        String type, status, guest;
+        int price;
+        DateTime occupiedUntil;
+        
+        public Booking(Database database, Users u)
+        {
+            this.u = u;
+            this.database = database;
+        }
+
+        public void status()
+        { //takes data about the room from the database(type, status(occupied or not), price and the date it is free from
+            //if the user is admin he can see the current guest of the room
+        }
+        public void reserve() { 
+          //when the user clicks on a room, something(smth smth dark side from WPF) will appear which will make a reservation
+        }
+
+
+    }
+}
